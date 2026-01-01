4daba599ebce2d20085b6c089f8991fa812f7b43
Muhammed <mmusabgldmr@gmail.com>
2026-01-01 12:48:38 +0300

    .
    .

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,3 +1,12 @@
-public class Vehicle {
+public interface Vehicle {
     
+    void hiziArtir(int miktar);
+    void hiziAzalt(int miktar);
+    int mevcutHiz();
+    String ozellikleriBastir();
+    int tekerlekSayisi();
+    int maximumHiz();
+    boolean guvenliMi();
+    void yaslandir();
+
 }
